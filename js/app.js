81c871e5ae071bfc64cc800c3329b417047a44ce
Raheem Abolfathzadeh <raheemabol91@gmail.com>
2022-05-23 16:26:41 -0400

    updated demo

---
+++ js/app.js
@@ -0,0 +1,16 @@
+let name = "Raheem";
+const name2 = "Jim";
+
+const num = 22;
+let bool = true;
+
+let msg = `Hey ${name} how are you and ${name2} doing??`;
+console.log(msg);
+
+let arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
+let stuff = [1, 2, 3, "four", true, null, 11, "11"];
+
+for (let i = 0; i < stuff.length; i++) {
+  console.log(stuff[i]);
+}
